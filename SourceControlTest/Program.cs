09ccb6788aa8986aa0f5ed909bf894ee05de80d7
Rob Wood <rdrwood@gmail.com>
2018-07-02 15:31:39 -0700

    crazy added

--- SourceControlTest/Program.cs
+++ SourceControlTest/Program.cs
@@ -10,7 +10,7 @@ namespace SourceControlTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello crazy World");
             Console.ReadKey();
         }
     }
