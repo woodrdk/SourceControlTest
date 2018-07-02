65184bf57c68a5cd00ed16a5b62fed9c6d6cfe08
Rob Wood <rdrwood@gmail.com>
2018-07-02 15:19:50 -0700

    Added a user greeting

--- SourceControlTest/Program.cs
+++ SourceControlTest/Program.cs
@@ -10,7 +10,8 @@ namespace SourceControlTest
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello World");
+            Console.ReadKey();
         }
     }
 }
